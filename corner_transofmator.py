3ab402642e0eb6b184e66f9898fbab9fc8db54f0
lukic4 <matea.lukic@ivii.eu>
2022-04-27 16:59:46 +0200

    "  added corner transformations script "

--- corner_transofmator.py
+++ corner_transofmator.py
@@ -1,11 +1,11 @@
 import os
 import cv2
-from cv2 import  perspectiveTransform
+from cv2 import perspectiveTransform
 from cv2 import transform
 import json
 import numpy as np
 from Configs import SharedConfigurations
-
+from PIL import Image, ImageDraw
 configs=SharedConfigurations()
 
 IRIIS_json=configs.IRIIS_json
@@ -83,22 +83,29 @@ examples=get_just_few_examples(siriusXiriis_corners)
 transformation_examples_path=r"D:\transformation_examples"
 saved_examples_path=r"D:\saved_examples"
 
+height_dif = 1024 - 800
+width_diff = 1360 - 1280
+
 def resize_images_and_save(path, saving_path):
     images=os.listdir(path)
+
     for im in images:
         image_path=path+"\\"+im
-        img = cv2.imread(image_path)
-        dimensions=img.shape   # IRIIS DIMS : (800, 1280, 3)  ;; SIRIUS DIMS : (1024, 1360, 3)  --- DIMS ARE ( HEIGHT, WIDTH)
-        extension=im[-9:]
+        img1 = Image.open(image_path)
+        img=img1
+        # IRIIS DIMS : (800, 1280, 3)  ;; SIRIUS DIMS : (1024, 1360, 3)  --- DIMS ARE ( HEIGHT, WIDTH)
+        extension = im[-9:]
         if(extension=="iriis.jpg"):
-            width = 1360
-            height = 1024
-            dim = (width, height)
-            img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-            dimensions = img.shape
-        print(dimensions)
+            img2 = img1.crop((0, 0, 1280, 800))
+            rect = Image.new("RGB", (1360, 1024))
+            Image.Image.paste(rect, img2, (0, 0))
+            #draw.rectangle((0, 0, 1360, 1024), fill='green')
+            img=rect
+        #dimensions = img.shape
+        #print(dimensions)
         filename=saving_path+"\\"+im
-        cv2.imwrite(filename, img)
+        img.save(filename)
+
 
 resize_images_and_save(transformation_examples_path, saved_examples_path)
 
@@ -106,33 +113,65 @@ resize_images_and_save(transformation_examples_path, saved_examples_path)
 wd=saved_examples_path
 
 def draw_sirius_corners_on_iriis_image(path,corners_dict):
-    pink = [199, 21, 133]
-    yellow = [255, 222, 173]
-    green = [64, 224, 208]
-    blue = [30, 144, 255]
 
+
+    red = 	[0,0,255]
+    yellow = [0,255,255]
+    green = [0,255,0]
+    blue = [255,0,0]
+
+    pink= [255,0,255]
+    thickness=20
+    radius=7
     images=os.listdir(path)
     for im in images:
         extension=im[-9:]
         if(extension=="iriis.jpg"):
             image_path = path + "\\" + im
             id=im.split("_")[0]
-            timestamp = id.split("-")[1]
-            if (id[0] == "4"):
-                id = id.split("-")[0][:-1] + "-" + timestamp  # id examle now : 4100010332-20210907T053847 # len(4100010332)=10 :)
+            #timestamp = id.split("-")[1]
+            #if (id[0] == "4"):
+            #    id = id.split("-")[0][:-1] + "-" + timestamp  # id examle now : 4100010332-20210907T053847 # len(4100010332)=10 :)
 
             if id in corners_dict:
                 sirius_edges=corners_dict[id][1]
                 sirius_x=sirius_edges[0]
                 sirius_y=sirius_edges[1]
-                print(id, "*******", sirius_edges,sirius_x,sirius_y)
-                a=sirius_x[0], sirius_y[0]
-                b=sirius_x[1], sirius_y[1]
-                c=sirius_x[2], sirius_y[2]
-                d=sirius_x[3], sirius_y[3]
+                #print(id, "*******", sirius_edges,sirius_x,sirius_y)
+                a= ( sirius_x[0], sirius_y[0] )
+                b= ( sirius_x[1], sirius_y[1] )
+                c= ( sirius_x[2], sirius_y[2] )
+                d= ( sirius_x[3], sirius_y[3] )
 
-            #img = cv2.imread(image_path)
-            #img = cv2.circle(img,centerOfCircle , 3, red, 3)
+                iriis_edges=corners_dict[id][0]
+                iriis_x=iriis_edges[0]
+                iriis_y=iriis_edges[1]
+                p1= ( iriis_x[0], iriis_y[0] )
+                p2= ( iriis_x[1], iriis_y[1] )
+                p3= ( iriis_x[2], iriis_y[2] )
+                p4= ( iriis_x[3], iriis_y[3] )
+
+            img = cv2.imread(image_path)
+            # image = cv.circle(image, centerOfCircle, radius, color, thickness)
+            #img = cv2.circle(img, a , radius, red, thickness)
+            #img = cv2.circle(img, b, radius,  yellow, thickness)
+            #img = cv2.circle(img, c, radius,  green, thickness)
+            #img = cv2.circle(img, d, radius,  blue, thickness)
+
+            img = cv2.circle(img, p1 , radius, pink, thickness)
+            img = cv2.circle(img, p2, radius,  pink, thickness)
+            img = cv2.circle(img, p3, radius,  pink, thickness)
+            img = cv2.circle(img, p4, radius,  pink, thickness)
+
+            pts1 = np.float32([[  iriis_x[0]  , iriis_y[0]], [iriis_x[1], iriis_y[1]],
+                               [iriis_x[2], iriis_y[2]], [ iriis_x[3], iriis_y[3] ]])
+            pts2 = np.float32([[sirius_x[0], sirius_y[0]], [sirius_x[1], sirius_y[1]],
+                               [sirius_x[2], sirius_y[2]], [sirius_x[3], sirius_y[3]]])
+            matrix = cv2.getPerspectiveTransform(pts1, pts2)
+            img= cv2.warpPerspective(img, matrix, (img.shape[1], img.shape[0]))
+
+            filename = path + "\\" + "edges_" + im
+            cv2.imwrite(filename, img)
 
 
 draw_sirius_corners_on_iriis_image(wd,examples)
