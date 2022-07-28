4d43e520a354ed890da9aa243806825db50fe86d
lukic4 <matea.lukic@ivii.eu>
2022-07-28 18:22:16 +0200

    " Started with Depth_Estimator.py"

---
+++ Depth_Estimator.py
@@ -0,0 +1,362 @@
+import os
+import sys
+import tensorflow as tf
+from tensorflow.keras import layers
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib
+import copy
+import matplotlib.pyplot as plt
+from Configs import SharedConfigurations
+
+config =SharedConfigurations()
+path_train=r"E:\DEPTH_ESTIMATOR\DATASET_train"
+path_validation=r"E:\DEPTH_ESTIMATOR\DATASET_validation"
+
+HEIGHT = 256
+WIDTH = 256
+LR = 0.0002
+EPOCHS = 5 # 30
+BATCH_SIZE = 32
+#### get data from folder ##########################
+def get_data(path):
+    filelist = []
+    files=os.listdir(path )
+    for file in files:
+        if(file[-4:]!="krdi"):
+            #print(file)
+            file_path=path  + "\\" + file
+            filelist.append(file_path)
+
+
+    filelist.sort()
+    data = {
+        "image": [x for x in filelist if x.endswith("-color.png")],
+        "depth": [x for x in filelist if x.endswith("-depth.png")],
+        #"mask": [x for x in filelist if x.endswith("-depth_mask.npy")],
+    }
+    return data
+
+
+data=get_data(path_validation)
+
+df = pd.DataFrame(data)
+df = df.sample(frac=1, random_state=42)
+
+########### build  pipeline ###########################
+
+
+class DataGenerator(tf.keras.utils.Sequence):
+    def __init__(self, data, batch_size=6, dim=(768, 1024), n_channels=3, shuffle=True):
+        """
+        Initialization
+        """
+        self.data = data
+        self.indices = self.data.index.tolist()
+        self.dim = dim
+        self.n_channels = n_channels
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.min_depth = 0.1
+        self.on_epoch_end()
+
+    def __len__(self):
+        return int(np.ceil(len(self.data) / self.batch_size))
+
+    def __getitem__(self, index):
+        if (index + 1) * self.batch_size > len(self.indices):
+            self.batch_size = len(self.indices) - index * self.batch_size
+        # Generate one batch of data
+        # Generate indices of the batch
+        index = self.indices[index * self.batch_size : (index + 1) * self.batch_size]
+        # Find list of IDs
+        batch = [self.indices[k] for k in index]
+        x, y = self.data_generation(batch)
+
+        return x, y
+
+    def on_epoch_end(self):
+
+        """
+        Updates indexes after each epoch
+        """
+        self.index = np.arange(len(self.indices))
+        if self.shuffle == True:
+            np.random.shuffle(self.index)
+
+    def load(self, image_path, depath_path ):
+        """Load input and target image."""
+
+        image_ = cv2.imread(image_path)
+        image_ = cv2.cvtColor(image_, cv2.COLOR_BGR2RGB)
+        image_ = cv2.resize(image_, self.dim)
+        image_ = tf.image.convert_image_dtype(image_, tf.float32)
+
+
+        depth_map = cv2.imread(depath_path)
+        depth_map = cv2.cvtColor(depth_map, cv2.COLOR_BGR2RGB)
+        depth_map = cv2.resize(depth_map, self.dim)
+        depth_map = tf.image.convert_image_dtype(depth_map, tf.float32)
+
+        return image_, depth_map
+
+    def data_generation(self, batch):
+
+        x = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size, *self.dim, self.n_channels))
+
+        for i, batch_id in enumerate(batch):
+            x[i,], y[i,] = self.load(
+                self.data["image"][batch_id],
+                self.data["depth"][batch_id],
+                #self.data["mask"][batch_id],
+            )
+
+        return x, y
+
+
+
+def visualize_depth_map(samples, test=False, model=None):
+    input, target = samples
+    cmap = copy.copy(matplotlib.cm.get_cmap("jet"))
+    cmap.set_bad(color="black")
+
+    if test:
+        pred = model.predict(input)
+        #fig, ax = plt.subplots(6, 3, figsize=(50, 50))
+        for i in range(4):
+            #plt.imshow((input[i].squeeze()))
+            #plt.show()
+            #plt.imshow((target[i].squeeze()), cmap=cmap)
+            #plt.show()
+            plt.imshow((pred[i].squeeze()), cmap=cmap)
+            plt.show()
+
+    else:
+        #fig, ax = plt.subplots(6, 2, figsize=(50, 50))
+        for i in range(4):
+            plt.imshow((input[i].squeeze()))
+            plt.show()
+            plt.imshow((target[i].squeeze()), cmap=cmap)
+            plt.show()
+
+data_gen=DataGenerator(data=df, batch_size=6, dim=(HEIGHT, WIDTH))
+visualize_samples  = next(iter(data_gen))
+
+#visualize_depth_map(visualize_samples)
+
+
+##################################################################################################################
+###################################### BUILD MODEL ############################################################
+###############################################################################################################
+
+class DownscaleBlock(layers.Layer):
+    def __init__(
+        self, filters, kernel_size=(3, 3), padding="same", strides=1, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.convA = layers.Conv2D(filters, kernel_size, strides, padding)
+        self.convB = layers.Conv2D(filters, kernel_size, strides, padding)
+        self.reluA = layers.LeakyReLU(alpha=0.2)
+        self.reluB = layers.LeakyReLU(alpha=0.2)
+        self.bn2a = tf.keras.layers.BatchNormalization()
+        self.bn2b = tf.keras.layers.BatchNormalization()
+
+        self.pool = layers.MaxPool2D((2, 2), (2, 2))
+
+    def call(self, input_tensor):
+        d = self.convA(input_tensor)
+        x = self.bn2a(d)
+        x = self.reluA(x)
+
+        x = self.convB(x)
+        x = self.bn2b(x)
+        x = self.reluB(x)
+
+        x += d
+        p = self.pool(x)
+        return x, p
+
+
+class UpscaleBlock(layers.Layer):
+    def __init__(
+        self, filters, kernel_size=(3, 3), padding="same", strides=1, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.us = layers.UpSampling2D((2, 2))
+        self.convA = layers.Conv2D(filters, kernel_size, strides, padding)
+        self.convB = layers.Conv2D(filters, kernel_size, strides, padding)
+        self.reluA = layers.LeakyReLU(alpha=0.2)
+        self.reluB = layers.LeakyReLU(alpha=0.2)
+        self.bn2a = tf.keras.layers.BatchNormalization()
+        self.bn2b = tf.keras.layers.BatchNormalization()
+        self.conc = layers.Concatenate()
+
+    def call(self, x, skip):
+        x = self.us(x)
+        concat = self.conc([x, skip])
+        x = self.convA(concat)
+        x = self.bn2a(x)
+        x = self.reluA(x)
+
+        x = self.convB(x)
+        x = self.bn2b(x)
+        x = self.reluB(x)
+
+        return x
+
+
+class BottleNeckBlock(layers.Layer):
+    def __init__(
+        self, filters, kernel_size=(3, 3), padding="same", strides=1, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.convA = layers.Conv2D(filters, kernel_size, strides, padding)
+        self.convB = layers.Conv2D(filters, kernel_size, strides, padding)
+        self.reluA = layers.LeakyReLU(alpha=0.2)
+        self.reluB = layers.LeakyReLU(alpha=0.2)
+
+    def call(self, x):
+        x = self.convA(x)
+        x = self.reluA(x)
+        x = self.convB(x)
+        x = self.reluB(x)
+        return x
+
+################################# LOSS DEFINITION #################################
+
+class DepthEstimationModel(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.ssim_loss_weight = 0.85
+        self.l1_loss_weight = 0.1
+        self.edge_loss_weight = 0.9
+        self.loss_metric = tf.keras.metrics.Mean(name="loss")
+        f = [16, 32, 64, 128, 256]
+        self.downscale_blocks = [
+            DownscaleBlock(f[0]),
+            DownscaleBlock(f[1]),
+            DownscaleBlock(f[2]),
+            DownscaleBlock(f[3]),
+        ]
+        self.bottle_neck_block = BottleNeckBlock(f[4])
+        self.upscale_blocks = [
+            UpscaleBlock(f[3]),
+            UpscaleBlock(f[2]),
+            UpscaleBlock(f[1]),
+            UpscaleBlock(f[0]),
+        ]
+        self.conv_layer = layers.Conv2D(3, (1, 1), padding="same", activation="tanh")
+
+    def calculate_loss(self, target, pred):
+        # Edges
+        dy_true, dx_true = tf.image.image_gradients(target)
+        dy_pred, dx_pred = tf.image.image_gradients(pred)
+        weights_x = tf.exp(tf.reduce_mean(tf.abs(dx_true)))
+        weights_y = tf.exp(tf.reduce_mean(tf.abs(dy_true)))
+
+        # Depth smoothness
+        smoothness_x = dx_pred * weights_x
+        smoothness_y = dy_pred * weights_y
+
+        depth_smoothness_loss = tf.reduce_mean(abs(smoothness_x)) + tf.reduce_mean(
+            abs(smoothness_y)
+        )
+
+        # Structural similarity (SSIM) index
+        ssim_loss = tf.reduce_mean(
+            1
+            - tf.image.ssim(
+                target, pred, max_val=WIDTH, filter_size=7, k1=0.01 ** 2, k2=0.03 ** 2
+            )
+        )
+        # Point-wise depth
+        l1_loss = tf.reduce_mean(tf.abs(target - pred))
+
+        loss = (
+            (self.ssim_loss_weight * ssim_loss)
+            + (self.l1_loss_weight * l1_loss)
+            + (self.edge_loss_weight * depth_smoothness_loss)
+        )
+
+        return loss
+
+    @property
+    def metrics(self):
+        return [self.loss_metric]
+
+    def train_step(self, batch_data):
+        input, target = batch_data
+        with tf.GradientTape() as tape:
+            pred = self(input, training=True)
+            loss = self.calculate_loss(target, pred)
+
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        self.loss_metric.update_state(loss)
+        return {
+            "loss": self.loss_metric.result(),
+        }
+
+    def test_step(self, batch_data):
+        input, target = batch_data
+
+        pred = self(input, training=False)
+        loss = self.calculate_loss(target, pred)
+
+        self.loss_metric.update_state(loss)
+        return {
+            "loss": self.loss_metric.result(),
+        }
+
+    def call(self, x):
+        c1, p1 = self.downscale_blocks[0](x)
+        c2, p2 = self.downscale_blocks[1](p1)
+        c3, p3 = self.downscale_blocks[2](p2)
+        c4, p4 = self.downscale_blocks[3](p3)
+
+        bn = self.bottle_neck_block(p4)
+
+        u1 = self.upscale_blocks[0](bn, c4)
+        u2 = self.upscale_blocks[1](u1, c3)
+        u3 = self.upscale_blocks[2](u2, c2)
+        u4 = self.upscale_blocks[3](u3, c1)
+
+        return self.conv_layer(u4)
+
+###################### TRAINING ##############################
+optimizer = tf.keras.optimizers.Adam(
+    learning_rate=LR,
+    amsgrad=False,
+)
+model = DepthEstimationModel()
+# Define the loss function
+cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction="none"
+)
+# Compile the model
+model.compile(optimizer, loss=cross_entropy)
+
+train_loader = DataGenerator(
+    data=df[:20].reset_index(drop="true"), batch_size=BATCH_SIZE, dim=(HEIGHT, WIDTH) # insted of 20 => should be 260
+)
+validation_loader = DataGenerator(
+    data=df[20:].reset_index(drop="true"), batch_size=BATCH_SIZE, dim=(HEIGHT, WIDTH) # insted of 20 => should be 260
+)
+model.fit(
+    train_loader,
+    epochs=EPOCHS,
+    validation_data=validation_loader,
+)
+
+#################################################################################
+
+test_loader = next(
+    iter(
+        DataGenerator(
+            data=df, batch_size=6, dim=(HEIGHT, WIDTH) # 265
+        )
+    )
+)
+visualize_depth_map(test_loader, test=True, model=model)
